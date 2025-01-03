8f4b34ed6dba0fb2c173e1bfeae5e19546c9d3fb
3lcode <3lcode@mail.ru>
2025-01-03 19:44:38 +0300

    Добавил изменения для сдачи работы

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import { createRoot } from 'react-dom/client';
-import { StrictMode, CSSProperties } from 'react';
+import { StrictMode, CSSProperties, useState } from 'react';
 import clsx from 'clsx';
 
 import { Article } from './components/article/Article';
@@ -12,20 +12,34 @@ import styles from './styles/index.module.scss';
 const domNode = document.getElementById('root') as HTMLDivElement;
 const root = createRoot(domNode);
 
+export type PageStyle = {
+	'--font-family': string;
+	'--font-size': string;
+	'--font-color': string;
+	'--container-width': string;
+	'--bg-color': string;
+};
+
 const App = () => {
+
+
+	const [articleStyles, setArticleStyles] = useState({
+		'--font-family': defaultArticleState.fontFamilyOption.value,
+		'--font-size': defaultArticleState.fontSizeOption.value,
+		'--font-color': defaultArticleState.fontColor.value,
+		'--container-width': defaultArticleState.contentWidth.value,
+		'--bg-color': defaultArticleState.backgroundColor.value,
+	});
+
+	const updateStyle = (style: PageStyle) => {
+		setArticleStyles(style);
+	  };
+	
 	return (
 		<div
 			className={clsx(styles.main)}
-			style={
-				{
-					'--font-family': defaultArticleState.fontFamilyOption.value,
-					'--font-size': defaultArticleState.fontSizeOption.value,
-					'--font-color': defaultArticleState.fontColor.value,
-					'--container-width': defaultArticleState.contentWidth.value,
-					'--bg-color': defaultArticleState.backgroundColor.value,
-				} as CSSProperties
-			}>
-			<ArticleParamsForm />
+			style={articleStyles as CSSProperties}>
+			<ArticleParamsForm updateStyle={updateStyle} />
 			<Article />
 		</div>
 	);
